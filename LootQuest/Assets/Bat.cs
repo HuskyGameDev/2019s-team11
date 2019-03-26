bf0717e1c91201838b9a205f2227f8d1daccf244
sjbollno <43500323+sjbollno@users.noreply.github.com>
2019-03-26 18:06:57 -0400

    added enemy stat script and started making enemies

---
+++ LootQuest/Assets/Bat.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bat : EnemyStatsScript
+{
+    public Transform target;
+    // Start is called before the first frame update
+    void Start()
+    {
+        target = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    void CheckDistance()
+    {
+        if(Vector3.Distance(target.position, transform.position) <= ChaseRange)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, target.position, MoveSpeed * Time.deltaTime);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CheckDistance();
+    }
+}
