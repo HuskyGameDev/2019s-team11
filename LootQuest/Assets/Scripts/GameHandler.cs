bad641f1c1b8b40990a90cd419769bc41d953c30
Sam Stevenson <samsteve@mtu.edu>
2019-03-26 18:16:55 -0400

    Healthbar scripts and gameobjects

---
+++ LootQuest/Assets/Scripts/GameHandler.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameHandler : MonoBehaviour
+{
+
+    [SerializeField] private Healthbar healthBar;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        healthBar.setSize(.4f);
+    }
+
+  
+}
