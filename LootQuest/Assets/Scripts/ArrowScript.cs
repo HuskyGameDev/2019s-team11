49fcc5046800cc254f1b21def5ecce2e2b063132
sjbollno <43500323+sjbollno@users.noreply.github.com>
2019-04-02 18:12:06 -0400

    moves my scripts into the correct folder and made arrow prefab

---
+++ LootQuest/Assets/Scripts/ArrowScript.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArrowScript : MonoBehaviour
+{
+    public int damage;
+    public bool player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+    }
+
+    private void OnTrigger(Collider2D collision)
+    {
+        if (player)
+        {
+            if (collision.gameObject.tag == "Enemy")
+            {
+                collision.gameObject.GetComponent<EnemyStatsScript>().Health -= damage;
+            }
+        }
+        else
+        {
+            if (collision.gameObject.tag == "Player")
+            {
+                //collision.gameObject.GetComponent<PlayerHealthScript>().Health -= damage;
+            }
+        }
+    }
+}
