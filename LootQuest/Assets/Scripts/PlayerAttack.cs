b7808ee6313bfa1410fd3c39cc438988a85adc70
Doug <daferrib@mtu.edu>
2019-04-02 19:00:47 -0400

    Attack and health for player
    Doesnt work!

---
+++ LootQuest/Assets/Scripts/PlayerAttack.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAttack : MonoBehaviour
+{
+    public GameObject meleeHitbox;
+    public float meleeDamage;
+    public float rangedDamage;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        meleeHitbox.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Attack();
+    }
+
+    private void Attack()
+    {
+        if (Input.GetButtonDown("MeleeAttack"))
+        {
+            meleeAttack();
+        }
+        if (Input.GetButtonDown("RangedAttack"))
+        {
+            rangedAttack();
+        }
+    }
+
+    private void meleeAttack()
+    {
+        //play animation
+        //spawn hitbox
+        meleeHitbox.SetActive(true);
+        //deal damage if something is in hitbox
+        
+        //despawn hitbox
+        meleeHitbox.SetActive(false);
+    }
+
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        if(col.tag == "Enemy")
+        {
+            damageEnemy(meleeDamage);
+        }
+    }
+
+    private void rangedAttack()
+    {
+        //play animation
+        //spawn projectile
+    }
+
+    private void damageEnemy(float damage)
+    {
+
+    }
+}
---
+++ LootQuest/Assets/Scripts/PlayerStats.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStats : MonoBehaviour
+{
+
+    public float playerHealth;
+    private float hitDelay = 0.5f;
+    public GameObject player;
+    private bool playerHit;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!playerHit)
+        {
+
+        }
+    }
+
+    private void applyDamage(int damage)
+    {
+        
+        playerHealth = playerHealth - damage;
+        if(playerHealth <= 0)
+        {
+            Die();
+        }
+    }
+
+    private void Die()
+    {
+        //play death animation
+        Destroy(player);
+    }
+}
