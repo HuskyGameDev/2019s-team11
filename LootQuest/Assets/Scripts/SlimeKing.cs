f95b9d75813d213d4b82752d109acf7f70bf74ba
sjbollno <43500323+sjbollno@users.noreply.github.com>
2019-04-12 15:33:46 -0400

    added basic boss mechanics
    moves towards player
    spawns slimes over time and upon death

---
+++ LootQuest/Assets/Scripts/SlimeKing.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SlimeKing : EnemyStatsScript
+{
+    public Transform target;
+    private float timer = 5f;
+    public Transform[] spawnpoints;
+    public Transform enemy;
+    // Start is called before the first frame update
+    void Start()
+    {
+        target = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    void Move()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, target.position, MoveSpeed * Time.deltaTime);
+    }
+
+    void CheckHealth()
+    {
+        if (Health <= 0)
+        {
+            //play death animation
+            //spawn loot?
+            //death effect?
+            Instantiate(enemy);
+            Instantiate(enemy);
+            Instantiate(enemy);
+            Instantiate(enemy);
+
+            Destroy(gameObject);
+        }
+    }
+
+    void CheckTimer()
+    {
+        timer = timer - Time.deltaTime;
+    }
+    void ResetTimer()
+    {
+        if (timer <= 0)
+        {
+            timer = 5f;
+        }
+    }
+
+    void SpawnSlimes()
+    {
+        Instantiate(enemy);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CheckHealth();
+        CheckTimer();
+        if (timer > 0)
+        {
+            Move();
+        }
+        else
+        {
+            SpawnSlimes();
+            ResetTimer();
+        }
+        
+
+    }
+}
