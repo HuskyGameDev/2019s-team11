ea43d8d11f0c6c5217db12abb7c0a0044120299b
Kevin Cornell <kdcornel@mtu.edu>
2019-04-13 19:23:33 -0400

    Dungeon Generation

---
+++ LootQuest/Assets/Scripts/DungeonGenerator-Copy.cs
@@ -0,0 +1,310 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class DungeonGenerator : MonoBehaviour
+{
+    [SerializeField]
+    private Tile groundTile;
+    [SerializeField]
+    private Tile pitTile;
+    [SerializeField]
+    private Tile topWallTile;
+    [SerializeField]
+    private Tile botWallTile;
+    [SerializeField]
+    private Tile rightWallTile;
+    [SerializeField]
+    private Tile leftWallTile;
+    [SerializeField]
+    private Tile ITRWallTile;
+    [SerializeField]
+    private Tile ITLWallTile;
+    [SerializeField]
+    private Tile IBRWallTile;
+    [SerializeField]
+    private Tile IBLWallTile;
+    [SerializeField]
+    private Tile OTRWallTile;
+    [SerializeField]
+    private Tile OTLWallTile;
+    [SerializeField]
+    private Tile OBRWallTile;
+    [SerializeField]
+    private Tile OBLWallTile;
+
+    [SerializeField]
+    private Tilemap groundMap;
+    [SerializeField]
+    private Tilemap pitMap;
+    [SerializeField]
+    private Tilemap wallMap;
+    [SerializeField]
+    private GameObject player;
+    [SerializeField]
+    private int deviationRate = 10;
+    [SerializeField]
+    private int roomRate = 15;
+    [SerializeField]
+    private int maxRouteLength;
+    [SerializeField]
+    private int maxRoutes = 20;
+
+    private int routeCount = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        int x = 0;
+        int y = 0;
+        int routeLength = 0;
+        GenerateSquare(x, y, 1);
+        Vector2Int previousPos = new Vector2Int(x, y);
+        y += 3;
+        GenerateSquare(x, y, 1);
+        NewRoute(x, y, routeLength, previousPos);
+
+        FillWalls();
+
+        FixCorners();
+    }
+
+    private void FixCorners()
+    {
+        BoundsInt bounds = groundMap.cellBounds;
+        for (int xMap = bounds.xMin - 10; xMap <= bounds.xMax + 10; xMap++)
+        {
+            for (int yMap = bounds.yMin - 10; yMap <= bounds.yMax + 10; yMap++)
+            {
+                Vector3Int pos = new Vector3Int(xMap, yMap, 0);
+                Vector3Int posBellow = new Vector3Int(xMap, yMap - 1, 0);
+                Vector3Int posAbove = new Vector3Int(xMap, yMap + 1, 0);
+                Vector3Int posRight = new Vector3Int(xMap - 1, yMap, 0);
+                Vector3Int posLeft = new Vector3Int(xMap + 1, yMap, 0);
+
+                TileBase tile = wallMap.GetTile(pos);
+                TileBase tileBellow = wallMap.GetTile(posBellow);
+                TileBase tileAbove = wallMap.GetTile(posAbove);
+                TileBase tileRight = wallMap.GetTile(posRight);
+                TileBase tileLeft = wallMap.GetTile(posLeft);
+
+                if ( tile == topWallTile )
+                {
+                    if ( tileAbove == rightWallTile && tileLeft == topWallTile )
+                    {
+                        wallMap.SetTile(pos, OBLWallTile);
+                    }
+                    else if ( tileAbove == leftWallTile && tileRight == topWallTile )
+                    {
+                        wallMap.SetTile(pos, OBRWallTile);
+                    }
+                }
+                if ( tile == botWallTile )
+                {
+                    if ( tileBellow == rightWallTile )
+                    {
+                        wallMap.SetTile(pos, OTLWallTile);
+                    }
+                    else if ( tileBellow == leftWallTile )
+                    {
+                        wallMap.SetTile(pos, OTRWallTile);
+                    }
+                }
+
+                //Checking for pit (filler) tiles
+                tile = pitMap.GetTile(pos);
+
+                if ( tile == pitTile )
+                {
+                    if ( tileAbove == rightWallTile && tileRight == botWallTile )
+                    {
+                        wallMap.SetTile(pos, IBRWallTile);
+                    }
+                    else if ( tileAbove == leftWallTile && tileLeft == botWallTile )
+                    {
+                        wallMap.SetTile(pos, IBLWallTile);
+                    }
+                    else if ( tileBellow == leftWallTile && tileLeft == topWallTile )
+                    {
+                        wallMap.SetTile(pos, ITLWallTile);
+                    }
+                    else if ( tileBellow == rightWallTile && tileRight == topWallTile )
+                    {
+                        wallMap.SetTile(pos, ITRWallTile);
+                    }
+                }
+
+                //Checking for wall tiles
+                tile = wallMap.GetTile(pos);
+                if ( tile == botWallTile)
+                {
+                    if (tileAbove == groundTile && tileRight == groundTile)
+                    {
+                        wallMap.SetTile(pos, OTRWallTile);
+                    }
+                    else if (tileAbove == groundTile && tileLeft == groundTile)
+                    {
+                        wallMap.SetTile(pos, OTLWallTile);
+                    }
+                }
+                else if (tile == topWallTile)
+                {
+                    if (tileBellow == groundTile && tileRight == groundTile)
+                    {
+                        wallMap.SetTile(pos, OBRWallTile);
+                    }
+                    else if (tileBellow == groundTile && tileLeft == groundTile)
+                    {
+                        wallMap.SetTile(pos, OBLWallTile);
+                    }
+                }
+            }
+        }
+    }
+
+    private void FillWalls()
+    {
+        BoundsInt bounds = groundMap.cellBounds;
+        for (int xMap = bounds.xMin-10; xMap <= bounds.xMax+10; xMap++)
+        {
+            for (int yMap = bounds.yMin - 10; yMap <= bounds.yMax + 10; yMap++)
+            {
+                Vector3Int pos = new Vector3Int(xMap, yMap, 0);
+                Vector3Int posBellow = new Vector3Int(xMap, yMap - 1, 0);
+                Vector3Int posAbove = new Vector3Int(xMap, yMap + 1, 0);
+                Vector3Int posRight = new Vector3Int(xMap - 1, yMap, 0);
+                Vector3Int posLeft = new Vector3Int(xMap + 1, yMap, 0);
+
+                TileBase tile = groundMap.GetTile(pos);
+                TileBase tileBellow = groundMap.GetTile(posBellow);
+                TileBase tileAbove = groundMap.GetTile(posAbove);
+                TileBase tileRight = groundMap.GetTile(posRight);
+                TileBase tileLeft = groundMap.GetTile(posLeft);
+
+                if (tile == null)
+                {
+                    pitMap.SetTile(pos, pitTile);
+                    if (tileBellow != null)
+                    {
+                        wallMap.SetTile(pos, topWallTile);
+                    }
+                    else if (tileAbove != null)
+                    {
+                        wallMap.SetTile(pos, botWallTile);
+                    }
+                    else if (tileRight != null)
+                    {
+                        wallMap.SetTile(pos, rightWallTile);
+                    }
+                    else if (tileLeft != null)
+                    {
+                        wallMap.SetTile(pos, leftWallTile);
+                    }
+                }
+
+            }
+        }
+    }
+
+    private void NewRoute(int x, int y, int routeLength, Vector2Int previousPos)
+    {
+        if (routeCount < maxRoutes)
+        {
+            routeCount++;
+            while (++routeLength < maxRouteLength)
+            {
+                bool routeUsed = false;
+                int xOffset = x - previousPos.x;
+                int yOffset = y - previousPos.y;
+                int roomSize = 1;
+                if (UnityEngine.Random.Range(1, 100) <= roomRate)
+                {
+                    roomSize = UnityEngine.Random.Range(3, 6);
+
+                }
+                previousPos = new Vector2Int(x, y);
+
+                //Go Straight
+                if (UnityEngine.Random.Range(1, deviationRate) == 1)
+                {
+                    if(routeUsed)
+                    {
+                        GenerateSquare(previousPos.x + xOffset, previousPos.y + yOffset, roomSize);
+                        NewRoute(previousPos.x + xOffset, previousPos.y + yOffset, UnityEngine.Random.Range(routeLength, maxRouteLength), previousPos);
+
+                    }
+                    else
+                    {
+                        x = previousPos.x + xOffset;
+                        y = previousPos.y + yOffset;
+                        GenerateSquare(x, y, roomSize);
+                        routeUsed = true;
+                    }
+                }
+
+                //Go Left
+                if (UnityEngine.Random.Range(1, deviationRate) == 1)
+                {
+                    if (routeUsed)
+                    {
+                        GenerateSquare(previousPos.x - yOffset, previousPos.y + xOffset, roomSize);
+                        NewRoute(previousPos.x - yOffset, previousPos.y + xOffset, UnityEngine.Random.Range(routeLength, maxRouteLength), previousPos);
+
+                    }
+                    else
+                    {
+                        y = previousPos.y + xOffset;
+                        x = previousPos.x - yOffset;
+                        GenerateSquare(x, y, roomSize);
+                        routeUsed = true;
+                    }
+                }
+
+                //Go Right
+                if (UnityEngine.Random.Range(1, deviationRate) == 1)
+                {
+                    if (routeUsed)
+                    {
+                        GenerateSquare(previousPos.x + yOffset, previousPos.y - xOffset, roomSize);
+                        NewRoute(previousPos.x + yOffset, previousPos.y - xOffset, UnityEngine.Random.Range(routeLength, maxRouteLength), previousPos);
+
+                    }
+                    else
+                    {
+                        x = previousPos.x + yOffset;
+                        y = previousPos.y - xOffset;
+                        GenerateSquare(x, y, roomSize);
+                        routeUsed = true;
+                    }
+                }
+
+                if (!routeUsed)
+                {
+                    x = previousPos.x + xOffset;
+                    y = previousPos.y + yOffset;
+                    GenerateSquare(x, y, roomSize);
+                }
+            }
+        }
+    }
+
+    private void GenerateSquare(int x, int y, int radius)
+    {
+        for (int tileX = x-radius; tileX <= x + radius; tileX++)
+        {
+            for (int tileY = y-radius; tileY <= y + radius; tileY++)
+            {
+                Vector3Int tilePos = new Vector3Int(tileX, tileY, 0);
+                groundMap.SetTile(tilePos, groundTile);
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ LootQuest/Assets/Scripts/DungeonGeneratorV2.cs
@@ -0,0 +1,336 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class DungeonGenerator1 : MonoBehaviour
+{
+    [SerializeField]
+    private Tile groundTile;
+    [SerializeField]
+    private Tile pitTile;
+    [SerializeField]
+    private Tile topWallTile;
+    [SerializeField]
+    private Tile botWallTile;
+    [SerializeField]
+    private Tile rightWallTile;
+    [SerializeField]
+    private Tile leftWallTile;
+    [SerializeField]
+    private Tile ITRWallTile;
+    [SerializeField]
+    private Tile ITLWallTile;
+    [SerializeField]
+    private Tile IBRWallTile;
+    [SerializeField]
+    private Tile IBLWallTile;
+    [SerializeField]
+    private Tile OTRWallTile;
+    [SerializeField]
+    private Tile OTLWallTile;
+    [SerializeField]
+    private Tile OBRWallTile;
+    [SerializeField]
+    private Tile OBLWallTile;
+
+    [SerializeField]
+    private Tilemap groundMap;
+    [SerializeField]
+    private Tilemap pitMap;
+    [SerializeField]
+    private Tilemap wallMap;
+
+    [SerializeField]
+    private int floorWidth = 10;
+    [SerializeField]
+    private int floorHeight = 10;
+    [SerializeField]
+    private int startX = 5;
+    [SerializeField]
+    private int startY = 5;
+
+    [SerializeField]
+    private int roomChance = 0;
+    [SerializeField]
+    private int chanceUp = 0;
+    [SerializeField]
+    private int chanceDown = -3;
+    [SerializeField]
+    private int chanceLeft = -7;
+    [SerializeField]
+    private int chanceRight = -9;
+
+    private string[,] floor;
+    private int totalRooms;
+    private int eX;
+    private int eY;
+    private string[,] path;
+    // Start is called before the first frame update
+    void Start()
+    {
+        createFloor();
+    }
+
+    void createFloor()
+    {
+        floor = new string[floorWidth, floorHeight];
+        for ( int y = 0; y<floorHeight; y++)
+        {
+            for ( int x = 0; x<floorWidth; x++)
+            {
+                floor[x,y] = " ";
+            }
+        }
+
+        floor[startX,startY] = "S";
+        totalRooms = 1;
+
+        genRooms(startX, startY);
+
+        genExit();
+
+        findExitPath(startX, startY);
+    }
+
+    private string[,] findExitPath(int startX, int startY)
+    {
+        path = new string[floorWidth,floorHeight];
+
+        for ( int y = 0; y < floorHeight; y++ )
+        {
+            for ( int x = 0; x < floorWidth; x++ )
+            {
+                path[x, y] = " ";
+            }
+        }
+
+        path[startX, startY] = "S";
+
+        createPath(path, startX, startY);
+
+        return path;
+    }
+
+    private void createPath(string[,] path, int xP, int yP)
+    {
+        //Vertical with Exit
+        if (eX == xP)
+        {
+            //Below, Above, Equal
+            if (eY < yP)
+            {
+                path[xP,yP - 1] = "X";
+                createPath(path, xP, yP - 1);
+            }
+            else if (eY > yP)
+            {
+                path[xP,yP + 1] = "X";
+                createPath(path, xP, yP + 1);
+            }
+            else
+            {
+                //They are on the exit
+                path[xP,yP] = "E";
+                return;
+            }
+        }
+        else if (eX > xP)
+        {
+            if (eY < yP)
+            {
+                //The exit is to the right and below
+                System.Random rand = new System.Random();
+                int chance = rand.Next(0, 2);
+                if (chance == 0)
+                {
+                    path[xP,yP - 1] = "X";
+                    createPath(path, xP, yP - 1);
+                }
+                else
+                {
+                    path[xP + 1,yP] = "X";
+                    createPath(path, xP + 1, yP);
+                }
+            }
+            else if (eY > yP)
+            {
+                //The exit is to the right and above
+                System.Random rand = new System.Random();
+                int chance = rand.Next(0, 2);
+                if (chance == 0)
+                {
+                    path[xP,yP + 1] = "X";
+                    createPath(path, xP, yP + 1);
+                }
+                else
+                {
+                    path[xP + 1,yP] = "X";
+                    createPath(path, xP + 1, yP);
+                }
+            }
+            else
+            {
+                path[xP + 1,yP] = "X";
+                createPath(path, xP + 1, yP);
+            }
+        }
+        else
+        {
+            if (eY < yP)
+            {
+                //The exit is to the left and below
+                System.Random rand = new System.Random();
+                int chance = rand.Next(0, 2);
+                if (chance == 0)
+                {
+                    path[xP,yP - 1] = "X";
+                    createPath(path, xP, yP - 1);
+                }
+                else
+                {
+                    path[xP - 1,yP] = "X";
+                    createPath(path, xP - 1, yP);
+                }
+            }
+            else if (eY > yP)
+            {
+                //The exit is to the left and above
+                System.Random rand = new System.Random();
+                int chance = rand.Next(0, 2);
+                if (chance == 0)
+                {
+                    path[xP,yP + 1] = "X";
+                    createPath(path, xP, yP + 1);
+                }
+                else
+                {
+                    path[xP - 1,yP] = "X";
+                    createPath(path, xP - 1, yP);
+                }
+            }
+            else
+            {
+                path[xP - 1,yP] = "X";
+                createPath(path, xP - 1, yP);
+            }
+        }
+    }
+
+    private void genExit()
+    {
+        Boolean exitFound = false;
+        System.Random rand = new System.Random();
+        int chance = 0;
+        while (!exitFound)
+        {
+            for( int y = 0; y < floorHeight; y++ )
+            {
+                for( int x = 0; x < floorWidth; x++ )
+                {
+                    if ( floor[x,y].Equals("X") && exitFound == false )
+                    {
+                        chance = rand.Next(0, floorWidth * floorHeight);
+                        if ( chance == 0 )
+                        {
+                            floor[x,y] = "E";
+                            eX = x;
+                            eY = y;
+                            exitFound = true;
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    void genRooms(int x, int y)
+    {
+        System.Random rand = new System.Random();
+        int chance = -1;
+
+        for( int i = 0; i < 4; i++ )
+        {
+            if ( i == 0 )
+            {
+                chance = rand.Next(0, roomChance + totalRooms) - chanceUp;
+                if ( chance <= 0)
+                {
+                    createRoom("UP", x, y);
+                }
+            }
+            else if ( i == 1 )
+            {
+                chance = rand.Next(0, roomChance + totalRooms) - chanceDown;
+                if (chance <= 0)
+                {
+                    createRoom("DOWN", x, y);
+                }
+            }
+            else if (i == 2)
+            {
+                chance = rand.Next(0, roomChance + totalRooms) - chanceLeft;
+                if (chance <= 0)
+                {
+                    createRoom("Left", x, y);
+                }
+            }
+            else
+            {
+                chance = rand.Next(0, roomChance + totalRooms) - chanceRight;
+                if (chance <= 0)
+                {
+                    createRoom("Right", x, y);
+                }
+            }
+        }
+
+    }
+
+    private void createRoom(string v, int x, int y)
+    {
+        if ( v.Equals("UP") )
+        {
+            if ( y < floorHeight && floor[x,y+1].Equals(" ") )
+            {
+                floor[x,y + 1] = "X";
+                totalRooms++;
+                genRooms(x, y + 1);
+            }
+        }
+        else if (v.Equals("DOWN"))
+        {
+            if ( y > 0 && floor[x,y - 1].Equals(" "))
+            {
+                floor[x,y - 1] = "X";
+                totalRooms++;
+                genRooms(x, y - 1);
+            }
+        }
+        else if (v.Equals("LEFT"))
+        {
+            if ( x > 0 && floor[x-1,y].Equals(" "))
+            {
+                floor[x-1,y] = "X";
+                totalRooms++;
+                genRooms(x-1, y);
+            }
+        }
+        else if (v.Equals("RIGHT"))
+        {
+            if (x < floorWidth && floor[x+1,y].Equals(" "))
+            {
+                floor[x+1,y] = "X";
+                totalRooms++;
+                genRooms(x+1, y);
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
