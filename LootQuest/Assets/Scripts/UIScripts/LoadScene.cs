15e3493add15dc13f6b628e17e781d0a45ff5df0
Doogie Warner <dgwarner@mtu.edu>
2019-04-13 19:58:15 -0400

    Add files via upload

---
+++ LootQuest/Assets/Scripts/UIScripts/LoadScene.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class LoadScene : MonoBehaviour {
+
+    //loads the scene based on the provided index
+    public void LoadByIndex(int sceneIndex)
+    {
+        SceneManager.LoadScene (sceneIndex);
+    }
+}
